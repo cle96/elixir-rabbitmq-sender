723957b9ec4aee96f81e400d2d0b694015810e6b
Cristian <cle@novicell.dk>
2019-05-20 14:04:41 +0200

    Added docker support

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM elixir:latest
+
+RUN mkdir /sender-app
+COPY . /sender-app
+WORKDIR /sender-app
+
+RUN mix local.rebar --force
+RUN mix local.hex --force
+RUN mix deps.compile
+
+CMD ["mix run", "/sender-app/sender.exs"]
--- sender.exs
+++ sender.exs
@@ -1,8 +1,19 @@
+defmodule Rabbit do
+	def loopMessages(channel) do
+	receive do
+		after
+	    	1_000 ->
+				AMQP.Basic.publish(channel, "", "catalog-worker", "Product [name]" |> Kernel.<>(DateTime.utc_now |> DateTime.to_string))
+				IO.puts (" Product [name] sent to catalog-worker" |> Kernel.<>(DateTime.utc_now |> DateTime.to_string))
+				loopMessages(channel)
+		end
+	end
+end
+
 {:ok, connection} = AMQP.Connection.open username: "user",
                                          password: "pass"
 
 {:ok, channel} = AMQP.Channel.open(connection)
 AMQP.Queue.declare(channel, "catalog-worker")
-AMQP.Basic.publish(channel, "", "catalog-worker", "Product [name]")
-IO.puts " Product [name] sent to catalog-worker"
-AMQP.Connection.close(connection)
+Rabbit.loopMessages(channel)
+AMQP.Connection.close(connection)	
